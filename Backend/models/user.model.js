fe416d4eb8292434102fecb0677339c0490c91ab
Aman Kumar Sahu <108629110+Coderaman12@users.noreply.github.com>
2024-12-17 01:46:14 +0530

    model Schema

---
+++ Backend/models/user.model.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const userSchema = new mongoose.Schema({
+    fullname:{
+        firstname:{
+            type: String,
+            required: true,
+            minlength: [3,"firstname must be at least 3 characters long"],
+            maxlength: 20
+        },
+        lastname:{
+            type: String,
+            minlength: [3,"lastname must be at least 3 characters long"],
+            maxlength: 20
+        } 
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password:{
+        type: String,
+        required: true,
+        select:false
+    },
+    socketId:{
+        type:String
+    }
+})
+
+userSchema.methods.generateAuthToken = function(){
+    const token = jwt.sign({_id:this._id},process.env.JWT_SECRET);    
+}
+
+userSchema.methods.comparePassword = async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword,this.password);
+}
+
+userSchema.statics.hashPassword = async function(password){
+    return await bcrypt.hash(password,10);
+}
+
+const userModel = mongoose.model('user',userSchema);
+
+module.exports = userModel;
--- projectbuild.md
+++ projectbuild.md
@@ -1,5 +1,4 @@
 --------- Node-Project-Build ----------
-
 1. npm init -y - > package.json create
 2.npm i express -> 
 3.create app.js -> main app
@@ -7,8 +6,14 @@
 5.here we have used the dotenv for port runnng
 [note- > make changes in package.json in main field to make server run]
 
-
 ----------------- Databse-Connection ----------------------
-1. npm i mongoose
-2.create [db/db.js]
-3.call the function from the db.js to app.js to connect the database
+-1. npm i mongoose
+-2.create [db/db.js]
+-3.call the function from the db.js to app.js to connect the database
+
+----- User-Model, User-Controllers -----
+-1.create [models/user.model.js] -> CREATE Schema and methods for comparing password
+-2.packgage 
+    -1.bcrypt - to hash the password 
+    -2.jwt - to generate token
+-3.create [controllers/controllers.js] to write the logic for the models we have created
